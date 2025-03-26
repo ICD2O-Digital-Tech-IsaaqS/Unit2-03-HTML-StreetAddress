1fa3c79cb7d314f5240cb1cdd0e087f20edbaed0
Isaaq Simon <IsaaqS>
2025-03-26 14:19:35 +0000

    Street

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function enterClicked () {
   // this function gets user's street number and street name and displays it back to user
 
   // get street number from text field
-  let firstName = document.getElementById("first-name").value
+  let streetnumber = document.getElementById("street-number").value
 	// get street name from text field and cast it to integer
-  let userAge = parseInt(document.getElementById("age-entered").value)
+  let streetname = document.getElementById("street-name").value
 
   // display street number and street name back to user
-  document.getElementById('user-info').innerHTML = "Your street number is " + firstName + " and your street name " + userAge + " ."
+  document.getElementById('user-info').innerHTML = "Your street number is " + streetnumber + " and your street name " + streetname + " . "
 }
